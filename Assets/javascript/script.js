37980db2e395ecf9d369fd651799a2bc20abd704
Chrisld50 <chrisld50@yahoo.com>
2022-09-07 19:33:01 -0500

    continued work on .js working on getting the data to pop up on the page

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -3,38 +3,68 @@ var cityWeather = document.querySelector('.city-weather');
 var cityText = document.querySelector('#city-text');
 var searchButton = document.querySelector(".searchBtn");
 const APIKey = "5292a248e8acdb206f3b3112df2113a7";
+
+
+
 function findCity(event){
     event.preventDefault();
-
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityText.value + "&appid=" + APIKey +'&units=imperial';
+  
+
     fetch(queryURL)
     .then(function (response) {
-        if (!response.ok) {
-            throw response.json();
-        }
-        return response.json();
-    
-    console.log(queryURL)
+        return response.json()
 
-    .then(function printResults(data){
-        for (let i = 0; i < data.length; i++) {
-           var cityCurrent = document.createElement('h2');
+    })
+
+    .then(function (data){
+         let latitude = data.coord.lat;
+         let longitude = data.coord.lon;
+        
+         console.log(latitude);
+         console.log(longitude)
+         let forecastURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}`;
+        let uvURL = 'http://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey + '&lat=' + latitude + '&lon=' + longitude;
+        
+         fetch(forecastURL)
+         .then(response => {
+             return response.json()
+         })
+         .then(forecastData => {
+             console.log(forecastData)
+             forecastData.list.map((element) => {
+                 console.log(element)
+             })
+         })
+        fetch(uvURL)
+        .then(function (response){
+            return response.json()
+        })
+console.log(data)
+            var cityCurrent = document.createElement('h2');
             var cityTemp = document.createElement('p');
             var cityWind = document.createElement('p');
             var cityHumidity = document.createElement('p');
             var cityUV = document.createElement('p');
-            cityCurrent.textContent = data[i].name;
-            cityTemp.textContent = data[i].temp;
-            cityWind.textContent = data[i].wind;
-            cityHumidity.textContent = data[i].humidity;
+            var cityImage = document.createElement('img'); 
+            cityCurrent.textContent = data.name, moment().format("MMM Do YY");
+            cityTemp.textContent = 'Temp: ' + data.main.temp + ' F';
+            cityWind.textContent = 'Wind: ' + data.wind.speed + ' MPH';
+            cityHumidity.textContent = 'Humidity: ' + data.main.humidity + ' %';
+            cityUV.textContent = 'UV: ' + data.value + ' %';
+            cityImage.src = "http://openweathermap.org/img/wn/" +  data.weather[0].icon; + "@2x.png";
             cityWeather.append(cityCurrent);
             cityWeather.append(cityTemp);
             cityWeather.append(cityWind);
             cityWeather.append(cityHumidity);
+            cityWeather.append(cityUV);
+            cityWeather.append(cityImage);   
+            console.log(cityImage)
+
             
             
-        }
-    }
+
+     })
 
 
     // pull: Temp, date, an image of what the weather is, wind, humidity and UV index for main.
